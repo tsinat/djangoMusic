68139cd529e8de3f6c949fbcec3796671e5fa05a
Tsinat Zeree <cheru87@gmail.com>
2017-03-13 16:49:06 -0700

    working sqlite models created

--- music/views.py
+++ music/views.py
@@ -1,7 +1,14 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>This is Music app homepage</h1>")
+    all_albums = Album.objects.all()
+    return render(request, 'music/index.html', {'all_albums': all_albums})
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id:" + str(album_id) + "</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
